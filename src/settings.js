b250bad9d9ba5ce5afea5723056daaa87ca04c65
Elliott Arnold <si3mshady@Elliotts-MBP.lan>
2024-04-01 17:21:37 -0500

    save point

---
+++ src/settings.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { AgoraVideoPlayer, createClient, createMicrophoneAndCameraTracks } from "agora-rtc-react";
+
+
+const APP_ID = "4045ca4346b742879dbde13e9e757f8b"
+const TOKEN = "007eJxTYLi2fr/N/HNHV3zpkVkY/7yjuKdH9Io72+as9eVP3ab8VDmmwGBiYGKanGhibGKWZG5iZGFumZKUkmponGqZam5qnmaR9EKHO60hkJEhInA3MyMDBIL4XAyJZYkliUWJOTmVDAwA2v4jvQ=="
+const CHANNEL = "avatarally"
