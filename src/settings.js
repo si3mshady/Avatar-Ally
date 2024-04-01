e95d2a384e87aa5c4a501f988874ce75d73616a7
Elliott Arnold <si3mshady@Elliotts-MBP.lan>
2024-04-01 17:42:27 -0500

    save point

--- src/settings.js
+++ src/settings.js
@@ -4,4 +4,8 @@ import { AgoraVideoPlayer, createClient, createMicrophoneAndCameraTracks } from 
 
 const APP_ID = "4045ca4346b742879dbde13e9e757f8b"
 const TOKEN = "007eJxTYLi2fr/N/HNHV3zpkVkY/7yjuKdH9Io72+as9eVP3ab8VDmmwGBiYGKanGhibGKWZG5iZGFumZKUkmponGqZam5qnmaR9EKHO60hkJEhInA3MyMDBIL4XAyJZYkliUWJOTmVDAwA2v4jvQ=="
-const CHANNEL = "avatarally"
+
+export const config = {mode: "rtc", codec: "vp8", appId: APP_ID, token: TOKEN }
+export const useClient  = createClient(config)
+export const useMicrophoneAndCameraTracks = createMicrophoneAndCameraTracks()
+export const CHANNEL = "avatarally";
