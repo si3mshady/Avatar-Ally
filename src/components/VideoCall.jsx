e6fcab2e7c29d8dfe73e14cfd22a6228055a71f1
Elliott Arnold <si3mshady@Elliotts-MBP.lan>
2024-04-01 20:43:00 -0500

    save point

--- src/components/VideoCall.jsx
+++ src/components/VideoCall.jsx
@@ -14,8 +14,42 @@ export default function VideoCall(props) {
 
     const {ready, tracks } = useMicrophoneAndCameraTracks()
 
-    useEffect(() => {}, [client, CHANNEL, ready, tracks])
-    
+    useEffect(() => {
+
+     client.on("user-unpublished", (user, mediaType) => {
+        if (mediaType === "audio") {
+            if (user.audioTrack) user.audioTrack.stop()
+        }
+
+        if (mediaType === "video") {
+
+            setUsers((previousUsers) => {
+                return previousUsers.filter((User) => User.uid !== user.id)
+            })
+        }
+     })   
+
+     client.on('user-published', async (user,mediaType) => {
+
+        await client.subscribe(user, mediaType)
+
+        if (mediaType === "video") {
+
+            setUsers((previousUsers) => {
+
+                return [...previousUsers, user]
+            })
+        }
+
+        if (mediaType === "audio") {
+
+            user.audioTrack.play()
+        }
+        
+     })
+
+    }, [client, CHANNEL, ready, tracks])
+
     
 
 
